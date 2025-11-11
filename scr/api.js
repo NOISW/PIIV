db9dd6270b83b838dc682fe85263b230434de864
NOISW <tecwcf@hotmail.com>
2025-11-11 19:48:25 +0000

    chore: removendo pasta scr incorreta

--- scr/api.js
+++
@@ -1,9 +0,0 @@
-export async function fetchEvasionData() {
-  return [
-    { year: 2021, evasion_rate: 12.5, total_students: 250 },
-    { year: 2022, evasion_rate: 10.8, total_students: 260 },
-    { year: 2023, evasion_rate: 11.2, total_students: 255 },
-    { year: 2024, evasion_rate: 9.9, total_students: 265 },
-    { year: 2025, evasion_rate: 8.5, total_students: 270 },
-  ];
-}
